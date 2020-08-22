1b64ac91d6ccd50783fe13e713303ecbf8848f18
Suman Basuli <thinisadhu@gmail.com>
2020-08-22 15:08:19 +0530

    Release v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DomFu',
-    version='1.0.0',
+    version='1.0.1',
     author='Suman Basuli',
     author_email='thinisadhu@gmail.com',
     packages=find_packages(),
